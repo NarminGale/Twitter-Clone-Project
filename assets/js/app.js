6c5d1c3b7b8e6d58a284040ed44956e599362d1d
NarminGale <narmingale@gmail.com>
2021-04-17 01:24:57 +0400

    Completed validation of inputs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,14 @@
-var inputMail = document.querySelector("#input-email");
-var inputPassword = document.querySelector("#input-password");
-var formControlMail = document.querySelector("#input-email .form-control");
-var formControlPas = document.querySelector("#input-password .form-control");
-var clickedSpanMail = document.querySelector("#input-email span");
-var clickedSpanPassword = document.querySelector("#input-password span");
+var email = document.querySelector('#exampleInputEmail1')
+var password = document.querySelector('#exampleInputPassword1')
+var btn = document.querySelector('#btn')
 
-
-var email = document.querySelector("#exampleInputEmail1")
-var password = document.querySelector("#exampleInputPassword1")
-var btn = document.querySelector("#btn")
-
-
-inputMail.addEventListener("click", function (e) {
-    if (e.currentTarget.classList.contains("mb-4")){
-        console.log (e.currentTarget.classList)
-
-        if (!formControlMail.classList.contains("show")) {
-            formControlMail.classList.add("show");
-            clickedSpanMail.classList.add("clicked-span");
-        }
-
-        inputMail.style.border = '2px solid #1DA1F2'; 
-        email.focus()
-    }  
-})
-
-
- 
-
-inputPassword.addEventListener("click", function () {
-
-    if (!formControlPas.classList.contains("show")) {
-        formControlPas.classList.add("show");
-        clickedSpanPassword.classList.add("clicked-span");
-       
-    
-        
-    }
-    inputPassword.style.border = '2px solid #1DA1F2'; 
-  
-password.focus()
-})
-
-
-
-
-email.addEventListener('input',buttonCheck); 
-password.addEventListener('input',buttonCheck)
+email.addEventListener('input', buttonCheck)
+password.addEventListener('input', buttonCheck)
 btn.disabled = true
-function buttonCheck (){
-
-if((email.value === "") || (password.value === "" ) ){
+function buttonCheck() {
+  if (email.value === '' || password.value === '') {
     btn.disabled = true
-    
-}else {
-  btn.disabled = false;
+  } else {
+    btn.disabled = false
+  }
 }
-}
-
-
