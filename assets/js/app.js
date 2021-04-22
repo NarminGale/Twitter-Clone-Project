87aa16bd86630a4a87f9e199485e59b3b10c9b6c
sssoltann <setvinnes@gmail.com>
2021-04-22 13:11:21 +0400

    Updated warning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,24 @@
 var email = document.querySelector('#exampleInputEmail1')
 var password = document.querySelector('#exampleInputPassword1')
 var btn = document.querySelector('#btn')
+var warning_div = document.querySelector('#warn');
 
 email.addEventListener('input', buttonCheck)
 password.addEventListener('input', buttonCheck)
-btn.disabled = true
+btn.disabled = true;
 function buttonCheck() {
+
   if (email.value === '' || password.value === '') {
-    btn.disabled = true
+    btn.disabled = true;
   } else {
-    btn.disabled = false
+    btn.disabled = false;
   }
 }
+
+btn.addEventListener("click",function(){
+  btn.disabled = true;
+  warning_div.classList.add('warning-div');
+  warning_div.innerHTML = "The username and password you entered did not match our records. Please double-check and try again.";
+  email.value = '';
+  password.value = '';
+});
