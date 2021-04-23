d4865017455b3a9a99459d99f0d6de3fba3928f7
NarminGale <narmingale@gmail.com>
2021-04-24 00:08:56 +0400

    Created js file

---
+++ assets/js/sign-up.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('select').addEventListener('focus', function (e) {
+  console.log(e.target)
+  //   document.querySelector('.upper-label').classList.remove('upper-label-color')
+})
