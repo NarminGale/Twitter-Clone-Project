f0f27866bc12f2c608873986440e821a17511230
sssoltann <setvinnes@gmail.com>
2021-04-25 18:11:41 +0400

    fixed border problem

--- assets/js/forgotpassword-app.js
+++ assets/js/forgotpassword-app.js
@@ -11,13 +11,17 @@ $('#search-button').on('click',function(){
     var value = $('#repair-input').val();
     if( value == '' ){
         // İnput yalnış seçilmiş görüntüsünü alır
-        $('#repair-input').addClass('wrong-input');
+        $('#repair-input').addClass('wrong-input'); 
+        $('#repair-input').css('border','1px solid #dd2e44');
         // Xəbərdarlığı çıxarır
         $('#warning-div').html('We need this information to find your account.');
     }else{
         // İnputu normal hala qaytarır
         $('#repair-input').removeClass('wrong-input');
+        $('#repair-input').css('border','1px solid #1da1f2');
         // Xəbərdarlığı yığışdırır
         $('#warning-div').html('');
     }
 });
+
+$
