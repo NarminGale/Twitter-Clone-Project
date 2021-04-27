82a1649813c204c3977379309c0228a4a10617b8
NarminGale <narmingale@gmail.com>
2021-04-28 01:11:02 +0400

    Created validation message

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -23,27 +23,102 @@ $('.use-Email').click(function () {
   }
 })
 
+$(document).ready(function () {
+  $('.name-input').focus(function () {
+    $('#name-label').css('color', '#1da1f2')
+    $('.name-input').css('border', '2px solid #1da1f2')
+  })
+})
+
 $('.name-input').focus(function () {
-  $('.limited-number').css('display', 'inline-block')
+  if ($('.name-input').val() === '') {
+    console.log($('.name-input').val())
+    $('#name-label').css('color', 'black')
+  }
 })
 $('.name-input').blur(function () {
   $('.limited-number').css('display', 'none')
+  $('#name-label').css('color', 'rgb(91, 112, 131)')
+  $('.name-input').css('border', '1px solid rgba(91, 112, 131, 0.425)')
+
+  if ($('.name-input').val() === '') {
+    $('.name-input').css('border', '1px solid rgb(224, 36, 94)')
+  }
 })
 
 let count = 0
 
-$('.name-input').keypress(function () {
+$('.name-input').keypress(function (event) {
+  count++
   $('.limited-number span').text(count)
   let a = $('.limited-number span').text()
-  if (parseInt(a) < 50) {
-    count++
-  } else if (parseInt(a) === 50) {
+  // if (parseInt(a) < 50) {
+  //   count++
+  // }
+  if (parseInt(a) > 50) {
+    count = 50
+    let a = $('.limited-number span').text('50')
     return false
   }
 })
-// $('.name-input').keydown(function (event) {
-//   var key = event.keyCode || event.charCode
 
-//   console.log(key)
+$('.name-input').keydown(function (event) {
+  if (event.key === 'Backspace' || event.key === 'Delete') {
+    count--
+    $('.limited-number span').text(count)
+    if (parseInt($('.limited-number span').text()) < 0) {
+      count = 0
+      let a = $('.limited-number span').text('0')
+      return false
+    }
+  }
+})
 
-// })
+// name validation
+$('.name-input').on('input', function () {
+  let userValue = $(this).val()
+  console.log(userValue)
+  if (userValue === '') {
+    $('#name-label').css('color', 'rgb(224, 36, 94)')
+    $('.name-input').css('border', '2px solid rgb(224, 36, 94)')
+    $('#validationServer04Feedback').css('display', 'block')
+  } else {
+    $('.name-input').css('border', '2px solid #1da1f2')
+    $('#validationServer04Feedback').css('display', 'none')
+  }
+})
+
+// phone validation
+
+$('input[type="tel"]').on('input', function () {
+  var userValue = $(this).val()
+  var phoneno = /^\d{10}$/
+  if (!userValue.match(phoneno)) {
+    $('input[type="tel"]').css('border', '2px solid rgb(224, 36, 94)')
+    $('.invalid-feedback-phone').css('display', 'block')
+    $('#phone-label').css('color', 'rgb(224, 36, 94)')
+  } else {
+    $('#phone-label').css('color', 'rgb(91, 112, 131)')
+    $('input[type="tel"]').css('border', '1px solid rgba(91, 112, 131, 0.425)')
+    $('.invalid-feedback-phone').css('display', 'none')
+  }
+})
+
+$('input[type="tel"]').blur(function () {
+  let phoneno = /^\d{9}$/
+  console.log($(this).val())
+  if (!$(this).val().match(phoneno)) {
+    $('input[type="tel"]').css('border', '2px solid rgb(224, 36, 94)')
+    $('.invalid-feedback-phone').css('display', 'block')
+    $('#phone-label').css('color', 'rgb(224, 36, 94)')
+  } else {
+    $('#phone-label').css('color', 'rgb(91, 112, 131)')
+    $('input[type="tel"]').css('border', '1px solid rgba(91, 112, 131, 0.425)')
+    $('.invalid-feedback-phone').css('display', 'none')
+  }
+  if ($(this).val() === '') {
+    $('#phone-label').css('color', 'rgb(91, 112, 131)')
+    $('input[type="tel"]').css('border', '1px solid rgba(91, 112, 131, 0.425)')
+    $('.invalid-feedback-phone').css('display', 'none')
+  }
+})
